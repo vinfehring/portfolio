b91709658166273d242b69e913e0c183379488d3
Vincenzo Fehring <vinfehring@gmail.com>
2024-06-19 12:47:18 -0500

    Initial commit from create-astro, enable node ssr adapter

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  server: {
+    host: "0.0.0.0",
+  },
+});
+
